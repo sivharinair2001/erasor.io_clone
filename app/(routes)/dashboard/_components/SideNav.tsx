00024bdaac31e38f2c8c62a75b36d03c4ffba754
SIV HARI NAIR <nairsivhari@gmail.com>
2024-09-20 02:45:23 +0530

    minor update

--- app/(routes)/dashboard/_components/SideNav.tsx
+++ app/(routes)/dashboard/_components/SideNav.tsx
@@ -1,35 +1,28 @@
-import { ChevronDown } from 'lucide-react'
+import { Archive, ChevronDown, Flag, Github } from 'lucide-react'
 import Image from 'next/image'
-import React, { use, useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import SideNavTopSection, { TEAM } from './SideNavTopSection'
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
 import SideNavBottomSection from './SideNavBottomSection'
 import { useConvex, useMutation } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 import { toast } from 'sonner'
-import { FileListContext } from '@/app/_context/FileListContext'
+import { FileListContext } from '@/app/_context/FilesListContext'
+
+
 
 function SideNav() {
-
-  const {user}= useKindeBrowserClient();
-  const [activeTeam,setActiveTeam]=useState<TEAM>();
-  const convex =useConvex();
-  const [totalFiles,setTotalFiles] = useState<Number>();
-  const {fileList_,setfileList_} =useContext(FileListContext);
+  const {user}:any=useKindeBrowserClient();
+  const createFile=useMutation(api.files.createFile);
+  const [activeTeam,setActiveTeam]=useState<TEAM|any>();
+  const convex=useConvex();
+  const [totalFiles,setTotalFiles]=useState<Number>();
+  const {fileList_,setFileList_}=useContext(FileListContext);
   useEffect(()=>{
     activeTeam&&getFiles();
   },[activeTeam])
-
-  const getFiles= async() =>{
-    const result= await convex.query(api.files.getFiles,{teamId:activeTeam?._id});
-
-    console.log(result);
-    setfileList_(result);
-    setTotalFiles(result.length);
-  }
-  const createFile = useMutation(api.files.createFile);
-  const onFileCreate =(fileName:String) =>{
-    console.log(fileName);
+  const onFileCreate=(fileName:string)=>{
+    console.log(fileName)
     createFile({
       fileName:fileName,
       teamId:activeTeam?._id,
@@ -38,24 +31,42 @@ function SideNav() {
       document:'',
       whiteboard:''
     }).then(resp=>{
-      if(resp) {
-        toast('File created successfully!!!');
+      if(resp)
+      {
+        getFiles();
+        toast('File created successfully!')
       }
     },(e)=>{
-      toast('Error creating file!!!');
+      toast('Error while creating file')
+
     })
   }
+
+  const getFiles=async()=>{
+    const result=await convex.query(api.files.getFiles,{teamId:activeTeam?._id});
+    console.log(result);
+    setFileList_(result);
+    setTotalFiles(result?.length)
+  }
+
   return (
-    <div className='border-[1px] h-screen fixed w-72 border-r p-6 flex flex-col'>
-        <div className='flex-1'>
-        <SideNavTopSection user={user}
-        setActiveTeamInfo={(activeTeam:TEAM)=>setActiveTeam(activeTeam)}/>
-        </div>
-        <div>
-          <SideNavBottomSection 
-          totalFiles={totalFiles}
-          onFileCreate={onFileCreate}/>
-        </div>
+    <div
+    className=' h-screen 
+    fixed w-72 borde-r border-[1px] p-6
+    flex flex-col
+    '
+    >
+      <div className='flex-1'>
+      <SideNavTopSection user={user} 
+      setActiveTeamInfo={(activeTeam:TEAM)=>setActiveTeam(activeTeam)}/>
+      </div>
+    
+     <div>
+      <SideNavBottomSection
+      totalFiles={totalFiles}
+      onFileCreate={onFileCreate}
+      />
+     </div>
     </div>
   )
 }
